2783f83a61cd2c26598297db16cf8d8c0731985a
ShacharHarshuv <shachar.harshuv@gmail.com>
2024-11-09 18:45:28 -0500

    Add analytics

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,9 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 export default function App() {
   return (
